d0ef72a8558853144f1714c65457d6c515b29c16
Dev-Ls-07 <166053318+Dev-Ls-07@users.noreply.github.com>
2024-06-27 21:02:33 -0300

    fazendo o contador

---
+++ desafioControleDeFluxo/contador.java
@@ -0,0 +1,10 @@
+public class contador {
+    public static void main(String[] args) {
+        int parametro1 = 12;
+        int parametro2 = 30;
+        int sum = parametro1+parametro2;
+        for(int i=0; i < parametro2; i++) {
+            System.out.println(i);
+        }
+    } 
+}
